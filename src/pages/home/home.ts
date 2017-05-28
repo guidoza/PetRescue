80a75cf3f8585438e8ef7ed83d08b9fce352e7e9
Guillermo <Guillermo@iMac-de-Guillermo.local>
2017-05-28 18:58:34 +0200

    Pet list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Platform, NavController, Loading, LoadingController } from 'ionic-angular';
 import { Auth, User } from '@ionic/cloud-angular';
 
 
@@ -8,9 +8,36 @@ import { Auth, User } from '@ionic/cloud-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-        
-  constructor(public auth:Auth, public navCtrl: NavController, public user: User) {
-    
+  petIcons: string[] = ["assets/dog.png", "assets/wolf.png", "assets/elephants.png"];
+  petList: Array<{title: string, description: string, icon: string}>;  
+  loading: Loading;
+
+  constructor(public auth:Auth, public navCtrl: NavController, public user: User, 
+              private loadingCtrl: LoadingController, public platform: Platform) {
+    this.petList = [];
+    for (let i = 0; i < 3; i++) {
+      this.petList.push({
+        title: 'Pet ' + i,
+        description: 'This is an incredible pet' + i,
+        icon: this.petIcons[i]
+      });
+    }  
+  }
+
+  public loadTimeline(refresher?) {
+    this.showLoading();
+    var load = this.loading;
+    setTimeout(function(){
+      load.dismiss();
+      refresher.complete();
+    }, 2000)
+  }
+
+  private showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    this.loading.present();
   }
 
 }
